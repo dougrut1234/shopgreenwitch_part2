ff0924bc4d2cd3beb589f3821309ccf1276ed16e
Doug Rutnarak <dougrut1234@aol.com>
2017-11-11 02:06:38 -0500

    changed landing altogether

--- js/home.js
+++ js/home.js
@@ -17,6 +17,35 @@ $(document).ready(function() {
             /* If the object is completely visible in the window, fade it it */
             if( bottom_of_window > bottom_of_object ){
                 
+                $(this).animate({'opacity':'1'},2000);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
+
+
+$(function(){  // $(document).ready shorthand
+  $('#brooklyn').fadeIn('slow');
+});
+
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('#brooklyn').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
                 $(this).animate({'opacity':'1'},1500);
                     
             }
@@ -27,18 +56,9 @@ $(document).ready(function() {
     
 });
 
-function getTimeRemaining(endtime) {
-  var t = Date.parse(endtime) - Date.parse(new Date());
-  var seconds = Math.floor((t / 1000) % 60);
-  var minutes = Math.floor((t / 1000 / 60) % 60);
-  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
-  return {
-    'total': t,
-    'days': days,
-    'hours': hours,
-    'minutes': minutes,
-    'seconds': seconds
-  };
-}
 
+$(document).ready(function() {
+        $('.meet_the_coven_nav').on('click', function() {
+            $("html, body").animate({ scrollTop: $('#bio').position().top }, 1000);
+        });
+    });
